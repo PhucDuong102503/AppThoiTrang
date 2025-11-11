cde51b668b9978e8249cda08b54cd6321c322fef
Phucdepzai <134357199+PhucDuong102503@users.noreply.github.com>
2025-11-11 13:05:34 +0700

    apk

--- app/src/main/java/com/example/fashionshopapp/util/Utils.java
+++ app/src/main/java/com/example/fashionshopapp/util/Utils.java
@@ -15,6 +15,7 @@ public class Utils {
     //public static final String BASE_URL = "http://192.168.1.9/FashionShop/";
     public static final String BASE_URL = "http://10.0.2.2/FashionShop/";
     //public static final String BASE_URL = "http://192.168.1.9/FashionShop/";
+    //public static final String BASE_URL = "http://phucdq.id.vn/api/";
 
 
     public static List<GioHang> manggiohang;
